aeee9315cf1a75c6a9730a8684b58559ebeff570
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-10-22 00:56:04 +0600

    create some new api

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 5000
 app.use(cors())
 
 const categoriesData = require('./data/categoryData.json')
+const news = require('./data/news.json')
+
 app.get('/', (req, res) =>{
     res.send('hello world!, i am goint to create a server for my application')
 })
@@ -13,6 +15,26 @@ app.get('/', (req, res) =>{
 
 app.get('/category', (req, res) =>{
     res.send(categoriesData)
+})
+
+app.get('/news/:id', (req, res) =>{
+    const id = req.params.id
+        const singleNews = news.find(ns => ns._id == id)
+        res.send(singleNews)
+})
+
+
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id 
+    if(id === '08'){
+        res.send(news)
+    }
+    const categories = news.filter(cat => cat.category_id === id)
+    res.send(categories)
+})
+
+app.get('/news', (req, res) =>{
+    res.send(news)
 })
 
 app.listen(port, ()=>{
